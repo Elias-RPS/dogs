c88f050bb8a2805934b2c7e6e937d2ed15a959a3
Elias-RPS <elias.perera@gmail.com>
2022-08-22 09:38:50 -0300

    Rotas de Login + Fetch inicial de autentcaçao

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import styles from './Login.module.css';
+import { Route, Routes } from 'react-router-dom';
+// import styles from './Login.module.css';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
